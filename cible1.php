d2133d7b04f005829dd7c328e30a1b1f6d1af46f
Mina Diallo <minadialloo5@gmail.com>
2019-02-15 14:56:07 +0000

    contact.php

---
+++ cible1.php
@@ -0,0 +1,54 @@
+
+<?php
+
+$personne=array(
+
+'nom'=>'Diallo',
+'prenom'=>'Aminata',
+'email'=>'minadialloo5@gmail.com',
+'adresse'=>'Baobab',
+'telephone'=>'776554277'
+
+);
+
+?>
+<!DDOCTYPE HTML>
+    <HTML lang='fr'>
+	  <head>
+	     <title>Mon formulaire </title>
+		  <style>
+		  table,tr,td,th{
+			  border:3px solid black ;
+			  border-radius:15px  15px 15px ;
+			  font-size:35px ;
+			  
+			  
+		  }
+		 body{ background:#E9967A;;
+		 }
+		 </style>
+		 </head>
+		 
+		 <body><center style=" padding-top:8%;">
+<table>
+<th colspan="4"><a href="na1.JPG"><img src="na1.JPG" 
+heigh : 5px ;
+width: 6px ;
+"/></a></th>
+		
+		<?php foreach ($personne as $key => $value): ?>
+<tr>
+<td><?php echo $key;?></td>
+<td><?php echo $value;?></td>
+</tr>
+<?php endforeach ?>
+</table>
+</center>
+</body>
+</html>
+
+
+	
+		 </body>
+		 </HTML>
+		 
